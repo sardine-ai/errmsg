780f36380299dae97f7700a9fab359b5aea3fb3f
Fabricio Viapiana <fabriciofviapiana@gmail.com>
2025-09-09 15:00:33 -0300

    fix(errmsg): handle unnamed types in JSON error messages
    Previously, Type.Name() returned empty strings for unnamed types like
    slices ([]string), maps, and channels, resulting in unclear error
    messages like "field should be  but received string".
    
    Now falls back to Type.String() when Name() returns empty, providing
    clear error messages like "field should be []string but received string".
    
    Fixes JSON unmarshaling error clarity for slice and other unnamed types.

--- errmsg_gpvalidator_test.go
+++ errmsg_gpvalidator_test.go
@@ -3,14 +3,18 @@ package errmsg_test
 import (
 	"encoding/json"
 	"errors"
+	"testing"
+
+	"bytes"
+
 	"github.com/daisy1754/errmsg"
 	errmsg_testing "github.com/daisy1754/errmsg/testing"
 	"github.com/go-playground/validator/v10"
-	"testing"
 )
 
 type request struct {
-	UserID string    `json:"userId" validate:"required"`
+	UserID string   `json:"userId" validate:"required"`
+	Names  []string `json:"names"`
 }
 
 func TestRequiredValidationError(t *testing.T) {
@@ -21,4 +25,20 @@ func TestRequiredValidationError(t *testing.T) {
 	err := validate.Struct(r)
 	errmsg_testing.AssertEquals(t, errmsg.Message(err), `'UserID' is a required field`)
 	errmsg_testing.AssertType(t, errors.Unwrap(errmsg.Wrap(err)), validator.ValidationErrors{})
+}
+
+func TestInvalidNamesTypeError(t *testing.T) {
+	var r request
+	input := `{"userId": "123", "names": "John Doe"}`
+	err := json.NewDecoder(bytes.NewReader([]byte(input))).Decode(&r)
+	errmsg_testing.AssertEquals(t, errmsg.Message(err), `'names' should be []string but received string`)
+	errmsg_testing.AssertType(t, errors.Unwrap(errmsg.Wrap(err)), &json.UnmarshalTypeError{})
+}
+
+func TestInvalidNamesValidationError(t *testing.T) {
+	var r request
+	input := `{"userId": "123", "names": 1}`
+	err := json.NewDecoder(bytes.NewReader([]byte(input))).Decode(&r)
+	errmsg_testing.AssertEquals(t, errmsg.Message(err), `'names' should be []string but received number`)
+	errmsg_testing.AssertType(t, errors.Unwrap(errmsg.Wrap(err)), &json.UnmarshalTypeError{})
 }
